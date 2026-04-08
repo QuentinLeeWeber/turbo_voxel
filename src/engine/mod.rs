c690f56e16ffc6dec3800218eb14f3c1d24a0ea4
Quentin Weber <mail@quentman.de>
2026-04-08 15:31:50 +0200

    add: very basic structure

--- src/engine/mod.rs
+++ src/engine/mod.rs
@@ -1,1 +1,29 @@
 pub mod renderer;
+mod scene;
+
+struct Transform {
+    pos: [f32; 3],
+    rot: [f32; 3],
+}
+
+enum HitBox {
+    None,
+    Sphere { radius: f32 },
+    Cube { size: f32 },
+}
+
+enum Event {
+    SpawObject(Box<dyn GameObject>),
+}
+
+trait GameObject {
+    fn get_id(&self) -> u32;
+    fn update(&mut self);
+    fn get_transform(&self) -> Transform;
+    fn get_hitbox(&self) -> HitBox;
+    fn notify(&mut self) -> Vec<Event>;
+}
+
+struct Engine {
+    scene: scene::Scene,
+}
