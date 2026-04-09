35cec8ea02dd146474f698761da930c5cad007fa
Thomas Steyer <t.steyer@campus.tu-berlin.de>
2026-04-09 18:47:28 +0200

    startet implementing collision detection

--- src/engine/mod.rs
+++ src/engine/mod.rs
@@ -66,6 +66,10 @@ enum HitBox {
         point2: [f32; 3],
         point3: [f32; 3],
     },
+    Line {
+        point: [f32; 3],
+        vec: [f32; 3],
+    },
 }
 impl HitBox {
     pub fn get_bounding_box(&self) -> Option<BoundingBox> {
@@ -110,6 +114,20 @@ impl HitBox {
                     z: CoordinateBorders::new(min_z, max_z),
                 })
             }
+            HitBox::Line { point, vec } => Some(BoundingBox {
+                x: CoordinateBorders::new(
+                    point[0].min(point[0] + vec[0]),
+                    point[0].max(point[0] + vec[0]),
+                ),
+                y: CoordinateBorders::new(
+                    point[1].min(point[1] + vec[1]),
+                    point[1].max(point[1] + vec[1]),
+                ),
+                z: CoordinateBorders::new(
+                    point[2].min(point[2] + vec[2]),
+                    point[2].max(point[2] + vec[2]),
+                ),
+            }),
         }
     }
 }
