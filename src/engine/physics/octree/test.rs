61597a2d0f94c3b84ea604896a1f25a181d638ae
Thomas Steyer <t.steyer@campus.tu-berlin.de>
2026-05-01 14:43:25 +0200

    added octree node idx mapping test

--- src/engine/physics/octree/test.rs
+++ src/engine/physics/octree/test.rs
@@ -1,4 +1,15 @@
 #[cfg(test)]
 mod octree_node {
-    use crate::engine::physics::*;
+    use super::super::*;
+    #[test]
+    fn idx_mapping_test() {
+        for i in 0..10000 {
+            let children = OctreeNode::get_idx_children(i);
+            for child in children {
+                assert_eq!(i, OctreeNode::get_idx_parent(child).unwrap());
+            }
+        }
+        assert_eq!(OctreeNode::get_idx_parent(0), None);
+        assert!(OctreeNode::get_idx_children(u64::MAX).len() == 0);
+    }
 }
